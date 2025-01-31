225230ea5c80ce8fd621f4f229ee0e75b420b2f0
Issame <i_canello@yahoo.com.br>
2025-01-30 22:01:34 -0300

    #

--- js/app.js
+++ js/app.js
@@ -1,6 +1,20 @@
 function comprar () {
     let tipo = document.getElementById('tipo-ingresso').value;
-    let qtd = document.getElementById('qtd').value;
+    let qtd = parseInt(document.getElementById('qtd').value);
 
-    
+    if(tipo == 'pista') {
+        comprarPista(qtd);
+
+    }
+}
+
+function comprarPista(qtd) {
+    let qtdPista = parseInt(document.getElementById('qtd-pista').textContent);
+    if (qtd > qtdPista) {
+        alert('Quantidade indisponível para pista');
+    } else {
+        qtdPista = qtdPista - qtd;
+        document.getElementById('qtd-pista').textContent = qtdPista;
+        alert('Compra efetuada com sucesso!');
+    }
 }
